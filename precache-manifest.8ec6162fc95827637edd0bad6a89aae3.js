daed6a42b5df516cfca5b52d2422bb4f2d58edf2
Carl Smith <carlfsmithiii@gmail.com>
2018-11-13 09:19:08 -0500

    Updates

---
+++ precache-manifest.8ec6162fc95827637edd0bad6a89aae3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5eb0563772f7c6732b3",
+    "url": "/github_card/static/css/main.5b073190.chunk.css"
+  },
+  {
+    "revision": "f5eb0563772f7c6732b3",
+    "url": "/github_card/static/js/main.f5eb0563.chunk.js"
+  },
+  {
+    "revision": "61b4854f117914a4fcec",
+    "url": "/github_card/static/js/1.61b4854f.chunk.js"
+  },
+  {
+    "revision": "c0ee22562f5722878c05",
+    "url": "/github_card/static/js/runtime~main.c0ee2256.js"
+  },
+  {
+    "revision": "f0ab5137dd277b810a57240e7b055e37",
+    "url": "/github_card/index.html"
+  }
+];

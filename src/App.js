ba4336672d538400a02ef78fe580e8e99b7b1f2d
Carl Smith <carlfsmithiii@gmail.com>
2018-11-12 22:55:58 -0500

    Move conditional user display from return of App.js render function to user variable in render function

--- src/App.js
+++ src/App.js
@@ -24,27 +24,26 @@ class App extends Component {
   }
 
   render() {
+    let user = null;
+    if (this.state.active) {
+      user = (
+        <div>
+          <img src={this.state.user.avatar_url} alt="abstract marker" />
+          <h2>{this.state.user.login}</h2>
+          <p>Type: {this.state.user.type}</p>
+          <p>
+            URL:{" "}
+            <a href={this.state.user.html_url}>{this.state.user.html_url}</a>
+          </p>
+        </div>
+      );
+    }
     return (
       <div>
         <button onClick={this.handleEvent}>
-          {this.state.active ? "Hide User" : "Show User"} 
+          {this.state.active ? "Hide User" : "Show User"}
         </button>
-        {
-          this.state.user.avatar_url && this.state.active && 
-          <img src={this.state.user.avatar_url} alt="abstract marker" />
-        }
-        {
-          this.state.user.login && this.state.active && 
-          <h2>{this.state.user.login}</h2>
-        }
-        {
-          this.state.user.type && this.state.active && 
-          <p>Type: {this.state.user.type}</p>
-        }
-        {
-          this.state.user.html_url && this.state.active && 
-          <p>URL: <a href={this.state.user.html_url}>{this.state.user.html_url}</a></p>
-        }
+        { user }
       </div>
     );
   }
